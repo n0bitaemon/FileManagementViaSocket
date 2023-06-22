a110460d66e272e473428e88bec1be4e5d043b31
nguyentuancuong20214950 <themagicradish@gmail.com>
2023-06-22 13:34:43 +0700

    log in out reg done

--- src/filemanager/com/server/cmd/auth/RegisterCommand.java
+++ src/filemanager/com/server/cmd/auth/RegisterCommand.java
@@ -6,32 +6,32 @@ import filemanager.com.server.cmd.validate.Validator;
 import filemanager.com.server.exception.InvalidNumberOfArgsException;
 import filemanager.com.server.exception.ServerException;
 import filemanager.com.server.exception.UserAlreadyExistException;
+import filemanager.com.server.exception.UserAlreadyLoginException;
 
 public class RegisterCommand extends Command {
 	@Override
 	public boolean validate() throws ServerException {
-		System.out.println("[SERVER LOG] REGISTER VALIDATION");
-		
-		if(!Validator.validateNumberOfArgs(getArgs(), 2)) {
+
+		if (!Validator.validateNumberOfArgs(getArgs(), 2)) {
 			throw new InvalidNumberOfArgsException(0, getArgs().size());
 		}
-		
-//		if(Authentication.channelIsLoging(Server.input[0])) {
-//			Server.output="u are already log in. pls log out first";
-//		}
-		
+
 		return true;
 	}
-	
+
 	@Override
 	public String exec() throws ServerException {
-		System.out.println("[SERVER LOG] REGISTER EXECUTION");
+		if (Authentication.channelIsLoging(getRemoteAddress())) {
+			throw new UserAlreadyLoginException(Authentication.session.get(getRemoteAddress()));
+		}
+
 		String username = this.getArgs().get(0);
-		if(Authentication.findAccInDatabase(username)) {
+		if (Authentication.findAccInDatabase(username)) {
 			throw new UserAlreadyExistException(username);
 		}
+		
 		Authentication.addAccountToDatabase(this.getArgs().get(0), this.getArgs().get(1));
-		return "Register success";
+		return String.format("Register account %s success", username);
 	}
 
 }
